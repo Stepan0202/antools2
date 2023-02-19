d5ca66cc779c90aa9e2766dce817fb1eb2b87637
Stepan0202 <stepan020293@gmail.com>
2023-02-19 18:43:50 +0200

    pagination in process

--- src/js/slider.js
+++ src/js/slider.js
@@ -3,6 +3,10 @@ const leftArrow = document.querySelector(".slider__left");
 const rightArrow = document.querySelector(".slider__right");
 const sliderItems = document.querySelectorAll(".slider__item");
 const itemContainer = document.querySelector(".slider__content");
+const paginationContainer = document.querySelectorAll(".slider__lines");
+const paginationElements = createElements("div", "slider__line", sliderItems.length);
+let sliderLineActive = "slider__line_active";
+let sliderDisabled = "slider__line_disabled";
 let currentPosition = 0;
 
 sliderItems.forEach((slide,indx) => {
@@ -10,7 +14,10 @@ sliderItems.forEach((slide,indx) => {
 });
 
 rightArrow.addEventListener("click", () => {
+    paginationElements[currentPosition].classList.remove(sliderLineActive);
+    paginationElements[currentPosition].classList.add(sliderDisabled);
     currentPosition++;
+    paginationElements[currentPosition].classList.add(sliderLineActive);
     if (currentPosition >= sliderItems.length) {
       currentPosition = 0;
     }
@@ -21,7 +28,10 @@ rightArrow.addEventListener("click", () => {
   });
   
   leftArrow.addEventListener("click", () => {
+    paginationElements[currentPosition].classList.remove(sliderLineActive);
+    paginationElements[currentPosition].classList.add(sliderDisabled);
     currentPosition--;
+    paginationElements[currentPosition].classList.add(sliderLineActive);
     if (currentPosition < 0) {
       currentPosition = sliderItems.length - 1;
     }
@@ -34,3 +44,12 @@ rightArrow.addEventListener("click", () => {
  step 1: 0%, 100%, 200%
  step
 */
+function createElements(type, elementClass, quantity){
+    let elements = [];
+    for (let index = 0; index < quantity; index++) {
+        let element = document.createElement(type);
+        element.classList.add(elementClass);
+        elements[index] = element;
+    }
+    return elements;
+}
