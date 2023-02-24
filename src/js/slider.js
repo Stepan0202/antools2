bd94fe50d7d8f718c20df89d566dc9a159b36f8e
Stepan0202 <stepan020293@gmail.com>
2023-02-24 23:20:51 +0200

    basic site is ready. Starting adaptive. Burger menu

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,26 +1,24 @@
 
+window.onload = init;
 const leftArrow = document.querySelector(".slider__left");
 const rightArrow = document.querySelector(".slider__right");
 const sliderItems = document.querySelectorAll(".slider__item");
 const itemContainer = document.querySelector(".slider__content");
-const paginationContainer = document.querySelectorAll(".slider__lines");
+const paginationContainer = document.querySelector(".slider__lines");
 const paginationElements = createElements("div", "slider__line", sliderItems.length);
 let sliderLineActive = "slider__line_active";
 let sliderDisabled = "slider__line_disabled";
 let currentPosition = 0;
 
-sliderItems.forEach((slide,indx) => {
-    slide.style = `transform: translateX(${indx * 100}%)`
-});
 
 rightArrow.addEventListener("click", () => {
     paginationElements[currentPosition].classList.remove(sliderLineActive);
     paginationElements[currentPosition].classList.add(sliderDisabled);
     currentPosition++;
-    paginationElements[currentPosition].classList.add(sliderLineActive);
     if (currentPosition >= sliderItems.length) {
       currentPosition = 0;
     }
+    paginationElements[currentPosition].classList.add(sliderLineActive);
     sliderItems.forEach((item, indx) => {
       const position = indx - currentPosition;
       item.style.transform = `translateX(${position * 100}%)`;
@@ -31,10 +29,10 @@ rightArrow.addEventListener("click", () => {
     paginationElements[currentPosition].classList.remove(sliderLineActive);
     paginationElements[currentPosition].classList.add(sliderDisabled);
     currentPosition--;
-    paginationElements[currentPosition].classList.add(sliderLineActive);
     if (currentPosition < 0) {
       currentPosition = sliderItems.length - 1;
     }
+    paginationElements[currentPosition].classList.add(sliderLineActive);
     sliderItems.forEach((item, indx) => {
       const position = indx - currentPosition;
       item.style.transform = `translateX(${position * 100}%)`;
@@ -44,6 +42,12 @@ rightArrow.addEventListener("click", () => {
  step 1: 0%, 100%, 200%
  step
 */
+function init(){
+    appendElements(paginationElements, paginationContainer);
+    sliderItems.forEach((slide,indx) => {
+        slide.style = `transform: translateX(${indx * 100}%)`
+    });
+}
 function createElements(type, elementClass, quantity){
     let elements = [];
     for (let index = 0; index < quantity; index++) {
@@ -52,4 +56,11 @@ function createElements(type, elementClass, quantity){
         elements[index] = element;
     }
     return elements;
+}
+function appendElements(elements, container){
+    elements.forEach((element, index) => {
+        container.appendChild(element);
+        if(index === currentPosition) element.classList.add("slider__line_active");
+        else element.classList.add("slider__line_disabled");
+    });
 }
